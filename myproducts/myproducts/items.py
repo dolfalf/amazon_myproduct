0b5b95e8de030e86d7daa1e6356deb82a1182b28
jaeeun <leejaeeun@archive-asia.co.jp>
2018-07-22 08:46:42 +0900

    first commit

---
+++ myproducts/myproducts/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MyproductsItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    thumbnail = scrapy.Field()
+    keyword = scrapy.Field()
+    detail_link = scrapy.Field()
+    page_count = scrapy.Field()
+    # pass
